91b32c6d153a1504299ed2fe74dfa99a04e7f59a
Manish Saini <mk81895@gmail.com>
2020-05-22 17:13:52 +0530

    set up Ejs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
  app.use('/', require('./routes'));
+
+ app.set('views', 'ejs');
+ app.set('views', './views');
 app.listen(port,function(error)
 {
        if(error)
