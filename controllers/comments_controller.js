5c553f73c37baddeb8317d6fbb910bf8a35d0042
Manish Saini <mk81895@gmail.com>
2020-06-06 15:49:56 +0530

    Now user Can delete Comments on his/her Post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -6,7 +6,7 @@ module.exports.create= function(req, res)
     {
         if(post)
         {
-            console.log(post + " post");
+           // console.log(post + " post");
             Comment.create({
                 content: req.body.content,
                 post: req.body.post,
@@ -23,4 +23,32 @@ module.exports.create= function(req, res)
         }
        // res.redirect('/');
     });                 
+}
+module.exports.destroy= function(req, res)
+{
+    console.log(" HERE I N DELETING COMMENTS ***************************" , req.body.params);
+    Comment.findById(req.params.id,function(error, comment)
+ {
+    let postId=comment.post;
+    // to allow the user to delete comment on his/her post
+    let findPost;
+    console.log(" I AM here 111"  , postId);
+    Post.findById(postId, function(error, post)
+    {
+            findPost=post;
+             console.log(" I AM here and"  , findPost.user._id);
+             console.log(" I am here and user id is", req.user.id);
+             if((comment.user==req.user.id ) || findPost.user._id ==req.user.id)
+             {
+                 
+     Post.findByIdAndUpdate(postId, {$pull: { comments :req.params.id }},function(error, post)
+     {
+         return res.redirect('back');
+     })  
+      }
+      else{ return res.redirect('back');}
+    });
+    // || (findPost.user.id==req.user.id)
+       
+ });
 }
--- controllers/users_post_controller.js
+++ controllers/users_post_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/post');
+const Comment=require('../models/comment');
 module.exports.post=function(req, res)
 {
     Post.create({
@@ -10,4 +11,22 @@ module.exports.post=function(req, res)
         if(error){console.log("Error in creating task"); return}
         return res.redirect('back');
        });
+}
+module.exports.destroy=  function(req, res)
+{
+    Post.findById(req.params.id,function(error, post)
+    {
+        // .id is written to convert the id into String for the Comparison
+        if(post.user == req.user.id)
+        {
+            post.remove();
+            Comment.deleteMany({post : req.params.id}, function(error)
+            {
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
 }
