7466bf7e1947c47de92f3468cb89ff371efb6abc
Manish Saini <mk81895@gmail.com>
2020-06-30 18:01:44 +0530

    Rendering Template

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -4,13 +4,14 @@ const nodemailer=require('../config/nodemailer');
 // this is the another way for exporting
 exports.newComment=(comment)=>
 {
-    console.log("I am herer", comment);
-    console.log(" INSIDE the comments");
+    let htmlString= nodemailer.renderTemplate({comment : comment}, 'comments/new_comment.ejs');
+    // console.log("I am herer", comment);
+    // console.log(" INSIDE the comments");
     nodemailer.transporter.sendMail({
         from:"Manish Saini",
         to:comment.user.email,
         subject:"New Comment Added",
-        html: '<h1>Yup  your comment is now published</h1>',
+        html: htmlString,
 
     },(error, info)=>
     {
